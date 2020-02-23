9d850b8599dc53a646aa0baf376c30773c76b619
wengwenyu <wengwenyu@aliyun.com>
2020-02-23 20:23:40 +0800

    括号匹配问题

--- 数据结构/02_stack_demo.py
+++ 数据结构/02_stack_demo.py
@@ -10,15 +10,47 @@ class Stack(object):
 
     def pop(self):
         """出栈"""
-        if len(self.stack) < 0:
+        if self.is_empty():
             return None
         return self.stack.pop()
 
     def get_top(self):
         """获取栈顶"""
-        if len(self.stack) < 0:
+        if self.is_empty():
             return None
         return self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+
+"""
+栈的应用: 括号匹配问题
+([{}])  匹配
+[{()}]  匹配
+()(  不匹配
+[{]} 不匹配
+"""
+
+
+def brace_match(s):
+    """匹配括号"""
+    match = {"}": "{", "]": "[", ")": "("}
+    stack = Stack()
+    for ch in s:
+        if ch in ["(", "[", "{"]:
+            stack.push(ch)
+        else:
+            if stack.is_empty():
+                return False
+            elif stack.get_top() == match[ch]:
+                stack.pop()
+            else:  # stack.get_top() != match[ch]
+                return False
+    if stack.is_empty():
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
@@ -27,3 +59,6 @@ if __name__ == "__main__":
     stack.push(2)
     stack.push(3)
     print(stack.pop())
+    print("==========括号匹配=========")
+    print(brace_match("[({})(){}]"))
+    print(brace_match("[]("))
