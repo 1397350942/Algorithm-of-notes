8d2cc3a5cde795f421a0af392e68e074b889954e
wengwenyu <wengwenyu@aliyun.com>
2020-02-26 16:30:38 +0800

    二叉搜索树删除

--- 数据结构/10_binary_search_tree.py
+++ 数据结构/10_binary_search_tree.py
@@ -95,6 +95,63 @@ class BST(object):
                 return p
         return None
 
+    def __remove_node_1(self, node):
+        """情况1 node是叶子节点"""
+        if not node.parent:  # 根节点
+            self.root = None
+        if node == node.parent.lchild:
+            """node是它父亲的左孩子"""
+            node.parent.lchild = None
+        else:  # node是它的父亲的右孩子
+            node.parent.rchild = None
+
+    def __remove_node_2_1(self, node):
+        """情况2.1: node只有一个左孩子"""
+        if not node.parent:
+            """如果是根节点"""
+            self.root = node.lchild
+            node.lchild.parent = None
+        elif node == node.parent.lchild:
+            node.parent.lchild = node.lchild
+            node.lchild.parent = node.parent
+        else:
+            node.parent.rchild = node.lchild
+            node.lchild.parent = node.parent
+
+    def __remove_node_2_2(self, node):
+        """情况2.2: node只有一个右孩子"""
+        if not node.parent:
+            self.root = node.rchild
+        elif node == node.parent.lchild:
+            node.parent.lchild = node.rchild
+            node.rchild.parent = node.parent
+        else:
+            node.parent.rchild = node.rchild
+            node.rchild.parent = node.parent
+
+    def delete(self, val):
+        if self.root:  # 不是空树
+            node = self.query_no_rec(val)
+            if not node:  # 不存在
+                return False
+            if not node.lchild and not node.rchild:  # 叶子节点
+                self.__remove_node_1(node)
+            elif not node.rchild:  # 2.1 只有一个左孩子
+                self.__remove_node_2_1(node)
+            elif not node.lchild:  # 情况 2.2 只有一个右孩子
+                self.__remove_node_2_2(node)
+            else:
+                # 情况3
+                min_node = node.rchild
+                while min_node.lchild:
+                    min_node = min_node.lchild
+                node.data = min_node.data
+                # 删除min_node
+                if min_node.rchild:
+                    self.__remove_node_2_2(min_node)
+                else:
+                    self.__remove_node_2_1(min_node)
+
 
 if __name__ == "__main__":
     tree = BST([4, 6, 7, 9, 2, 1, 3, 5, 8])
@@ -103,4 +160,6 @@ if __name__ == "__main__":
     tree.in_order(tree.root)
     print("")
     tree.post_order(tree.root)
-    print(tree.query_no_rec(4))
+    
+    tree.delete(4)
+    tree.post_order(tree.root)
